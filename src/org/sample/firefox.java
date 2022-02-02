4c253296a05f36636b31a489f2eac82eb2ace69d
ajithd209 <dramesh7871@gmail.com>
2022-02-02 09:56:45 +0530

    Hii

---
+++ src/org/sample/firefox.java
@@ -0,0 +1,26 @@
+package org.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class firefox {
+public static void main(String[] args) throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator.PC1\\eclipse-workspace\\S1\\driver\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("http://greenstech.in/selenium-course-content.html");
+WebElement txtid = driver.findElement(By.xpath("//a[@aria-controls='contact']"));
+txtid.click();
+
+WebElement txtname = driver.findElement(By.xpath("//h3[contains(text(),'Best Selenium Training')]"));
+String text = txtname.getText();
+System.out.println(text);
+
+
+}
+}
